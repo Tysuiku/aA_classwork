74a9fea14a3cf983325e89b06e656383c008da13
TimothyDong <timothydong41@gmail.com>
2022-09-29 19:50:11 -0400

    practice assessment stuff

---
+++ week 1/practiceAssessment1/problems/1_even_range.js
@@ -0,0 +1,25 @@
+/*******************************************************************************
+Write a function evenRange(start, end) that returns an array containing all even
+numbers between 'start' and 'end' in sequential order.
+
+Examples:
+
+evenRange(13, 20) => [ 14, 16, 18, 20 ]
+evenRange(4, 11) => [ 4, 6, 8, 10 ]
+evenRange(8, 5) => []
+*******************************************************************************/
+
+function evenRange(start, end) {
+  let evenArray = [];
+  for (i = start; i < end; i++){
+    if (i % 2 === 0){
+      evenArray.push(i)
+    } else {
+      continue;
+    }
+  }
+  return evenArray;
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
+module.exports = evenRange;
