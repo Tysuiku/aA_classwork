e17f410129dc36f40cf9d769e9bf1a09defd278e
TimothyDong <timothydong41@gmail.com>
2022-10-10 20:14:07 -0400

    assessment 4

---
+++ week 3/A4/problems/4_silly_cipher.js
@@ -0,0 +1,34 @@
+/*******************************************************************************
+Write a function sillyCipher(sentence, cipher) that takes in an string (sentence)
+and an object (cipher). Return a string where every character is replaced with its
+corresponding value in the cipher. If the character doesn't exist in the
+cipher, use a dot (.)
+
+Examples:
+
+sillyCipher("apple" , { a : "c", p : "x", l : "r", e : "o"}) //=> 'cxxro'
+sillyCipher("apple", { a : "c", p : "x"}) //=> 'cxx..'
+sillyCipher("bootcamp prep?", { o : "e", p : "q" , "?" : "!"}) //=> '.ee....q.q..q!'
+sillyCipher("twmce", { m : "n", t : "d", w : "a"}) //=> 'dan..''
+*******************************************************************************/
+function sillyCipher(words, cipher){
+  let word = '';
+  for(let i = 0; i < words.length; i++){
+    let char = words[i];
+      if(cipher[char]){
+        word += cipher[char];
+     } else {
+      word += '.';
+     }
+}
+return word;
+}
+
+
+
+//console.log(sillyCipher("apple" , { a : "c", p : "x", l : "r", e : "o"})); //=> 'cxxro'
+//console.log(sillyCipher("apple", { a : "c", p : "x"})); //=> 'cxx..'
+//console.log(sillyCipher("bootcamp prep?", { o : "e", p : "q" , "?" : "!"})); //=> '.ee....q.q..q!'
+//console.log(sillyCipher("twmce", { m : "n", t : "d", w : "a"})); //=> 'dan..''
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
+module.exports = sillyCipher;
