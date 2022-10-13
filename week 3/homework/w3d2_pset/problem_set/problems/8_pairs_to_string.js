f99e1e981cda40cca7db3049c09128ac50d55461
Tysuiku <Timothydong41@gmail.com>
2022-10-12 23:07:15 -0400

    hw3

--- week 3/homework/w3d2_pset/problem_set/problems/8_pairs_to_string.js
+++ week 3/homework/w3d2_pset/problem_set/problems/8_pairs_to_string.js
@@ -23,19 +23,33 @@ pairsToString(array2); // => 'food!'
 ***********************************************************************/
 
 function pairsToString(arr) {
-  var str = '';
-
-  for (var i = 0; i < arr.length; i += 1) {
-    var pair = arr[i];
-    var char = pair[0];
-    var num = pair[1];
-    for (var j = 0; j < num; j += 1) {
-      str += char;
-    }
+  let str = '';
+  for(let i = 0; i < arr.length; i++){
+      let subArr = arr[i];
+      let char = subArr[0]
+      let num = subArr[1]
+        for(let j = 0; j < num; j++){
+           str += char;
+        }
   }
-
   return str;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = pairsToString;
+var array1 = [
+  ['a', 3],
+  ['b', 1],
+  ['c', 2]
+];
+
+console.log(pairsToString(array1)); // => 'aaabcc'
+
+var array2 = [
+  ['f', 1],
+  ['o', 2],
+  ['d', 1],
+  ['!', 1]
+];
+
+console.log(pairsToString(array2)); // => 'food!'
