2dead3bafb8d52ec116a039cfdf820adc1c64b04
TimothyDong <timothydong41@gmail.com>
2022-10-13 20:25:50 -0400

    hwclasswork

---
+++ week 3/homework/w3d4_pset/problem_set/problems/1_odd_words_out.js
@@ -0,0 +1,26 @@
+/***********************************************************************
+Write a function oddWordsOut(sentence) that takes in a sentence string
+and returns the sentence where words with an odd number of characters
+are removed.
+
+Examples:
+
+oddWordsOut('go to the store and buy milk'); // => 'go to milk'
+oddWordsOut('what is the answer'); // => 'what is answer'
+***********************************************************************/
+
+function oddWordsOut(sentence) {
+    let words = sentence.split(' ');
+    let evens = [];
+    for(let i = 0 ; i < words.length; i++){
+        let word = words[i]
+        if(word.length % 2 === 0){
+            evens.push(word);
+        }
+    }
+    return evens.join(' ');
+}
+console.log(oddWordsOut('go to the store and buy milk')); // => 'go to milk'
+console.log(oddWordsOut('what is the answer')); // => 'what is answer'
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = oddWordsOut;
