8172beffd88783a54c49b715942cbf0252414247
TimothyDong <timothydong41@gmail.com>
2022-10-13 20:25:37 -0400

    classwork and hw for thursday

---
+++ week 3/classwork/classWork3.js
@@ -0,0 +1,33 @@
+// Data Modeling 
+// Q: What is at the heart of computer science?
+// A: How we represent our information!!!
+// Ex: Array[], Objects {}, trees,etc ...
+let arr = [1,2,3,4,5];
+//* use arrays to store ordered colletion of elements
+let student = ['alice', 'female', '63'];
+// to get the students name ..... student[0] student gender ..... student[1]
+//Q: How could we store the student's info more effiectively?
+//A: let student = {name: 'akice', gender: 'female'.........} with an object
+//Now to get name, instead of students[0] we use student['name'] or student.name
+let names = ['alice', 'bob', 'carol', 'dave'];
+//Take Away: If your data needs to be ordered then use a array otherwise use an object! 
+//(in some cases ... you'll figure out as you code)
+
+//How should we model these? (i.e array or object?)
+// 1) student 2) class of students) 3) car 4) parking lot
+// 1: object  2: array of objects  3: object  4: array of objects 
+
+let student1 = { name: 'jon', 'grade': 2};
+let student2 = { name: 'sam', 'grade': 4};
+let student3 = { name: 'joe', 'grade': 12};
+
+let class1 = [student1, student2, student3];
+console.log(class1);
+for(let student of class1){
+    console.log(student)
+}
+
+//list = ['rex','tim','ana']
+//for( let i of list){
+//    console.log(i)
+//}
---
+++ week 3/homework/w3d4_pset/problem_set/problems/2_has_all_vowels.js
@@ -0,0 +1,25 @@
+/***********************************************************************
+Write a function `hasAllVowels(str)` that takes in a string and returns
+true if the string contains every vowel (a, e, i, o, u) and false
+otherwise.
+
+Examples:
+
+hasAllVowels('have you gone biking?'); // => true
+hasAllVowels('get out of the way, silly'); // => true
+hasAllVowels('bootcamp prep'); // => false
+hasAllVowels('hello world'); // => false
+***********************************************************************/
+
+function hasAllVowels(str) {
+    let vowel = 'aeiou';
+    for(let i = 0; str.length; i++){
+        
+    }
+}
+hasAllVowels('have you gone biking?'); // => true
+hasAllVowels('get out of the way, silly'); // => true
+hasAllVowels('bootcamp prep'); // => false
+hasAllVowels('hello world'); // => false
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = hasAllVowels;
